b00becb93030f6bf746014bcc36dd6ff755f8294
shuall <shualloret@gmail.com>
2016-12-05 23:50:18 -0500

    mute, togglelayout, css fix for vert

--- stream.js
+++ stream.js
@@ -16,11 +16,11 @@ function ytframe(id, pid) {
 		console.log("yt not ready yet")
 		return
 	}
-	new YT.Player(pid, {
+	player = new YT.Player(pid, {
 		height: '',
 		width: '',
 		videoId: id,
-		playerVars: { 'disablekb': 1, 'controls': 0 },
+		playerVars: { 'disablekb': 1/*, 'controls': 0 */},
 		events: {
 			'onReady': onPlayerReady,
 			'onStateChange': onPlayerStateChange
@@ -37,6 +37,7 @@ function ytframe(id, pid) {
 			console.log("pschange: ", event)
 		}
 	}
+	return player
 }
 
 function getytplayerid() {
@@ -52,7 +53,41 @@ function addyt(id) {
 	div=document.createElement("div")
 	div.id=getytplayerid()
 	str.appendChild(div)
+	str.mute = function(m) { 
+		if(this.player == null || this.player.mute == null) return
+		if(arguments.length === 0) 
+			m = !this.player.isMuted()
+		if(m)
+			this.player.mute()
+		else
+			this.player.unMute()
+	}
+	str.islive = true;
+	str.play = function(p) {
+		if(this.player == null) return
+		if(arguments.length === 0)
+			switch(this.player.getPlayerState()) {
+			case YT.PlayerState.PAUSED:
+			case YT.PlayerState.ENDED:
+			case YT.PlayerState.CUED:
+				p=true
+				break
+			case YT.PlayerState.BUFFERING:
+			case YT.PlayerState.PLAYING:
+				p=false
+				break
+			case YT.PlayerState.UNSTARTED:
+				return
+			}
+
+		if(p) {
+			if(this.islive)
+				this.player.seekTo(0)
+			this.player.playVideo()
+		} else
+			this.player.pauseVideo()
+	}
 	addstream(str)
-	ytframe(id, div.id)
+	str.player = ytframe(id, div.id)
 	document.getElementById(div.id).tabindex="-1"
 }
